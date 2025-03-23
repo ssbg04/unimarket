4fdd121fe31851b9920d1facebd40debc446907f
ssbg04 <143066036+ssbg04@users.noreply.github.com>
2025-03-23 17:50:47 +0800

    project for IPT

---
+++ Unimarket/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>UniMarket</title>
+        <link rel="stylesheet" href="design/home-style.css">
+        <script src="javascript/home-script.js"></script>
+    
+</head>
+<body>
+    
+    <header>
+            <h1 style="color: orangered; display: inline-block;" >Uni</h1>
+            <h1 style="display: inline-block;" >Market</h1>
+            <nav>
+                <a href="#home" >Home</a>
+                <a href="#about">About</a>
+                <a href="#services">Services</a>
+                <a href="#contact">Contact</a>
+            </nav>
+        </header>
+        
+        <main>
+
+            <div id="home">
+                <h2>Welcome to UniMarket</h2>
+                <p>Your one-stop shop for school uniforms!</p>
+                <button id ="shop_now-button">Shop Now</button>
+            </div>
+
+            
+            
+            
+            
+            
+        </main>
+    </body>
+    </html>
